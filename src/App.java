649e7060f06ca4bcdf5ab9efb6676ff2dc77f210
Aqib <aqibdeen51211@gmail.com>
2025-02-08 21:42:05 +0000

    Enhance App.java with additional educational output about Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("I'm Tealtie's first Java program.");
+        System.out.println("He's learning Java.");
+        System.out.println("Java is a high-level, class-based, object-oriented programming language.");
+        System.out.println("Java is designed to have as few implementation dependencies as possible.");
+        System.out.println("Java is a general-purpose programming language intended to let application developers write once, run anywhere (WORA).");
+        System.out.println("It's pretty cool!");
     }
 }
